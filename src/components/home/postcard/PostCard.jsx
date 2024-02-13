3995860ac860f53e5476b19bd474479ce7bba55c
Abhishek <suryaabhishek329@gmail.com>
2024-02-13 20:11:16 +0530

    some small changes

--- src/components/home/postcard/PostCard.jsx
+++ src/components/home/postcard/PostCard.jsx
@@ -14,7 +14,7 @@ import { LoginContext } from '../../../context/Login';
 
 
 const PostCard = ({setIsFullScreen}) => {
-    const [selectedFile, setSelectedFile] = useState(null);
+    // const [selectedFile, setSelectedFile] = useState(null);
 
     const loginObj = useContext(LoginContext)
 
@@ -22,11 +22,11 @@ const PostCard = ({setIsFullScreen}) => {
         setIsFullScreen(false)
       }
 
-      const handleFileChange = (event) =>{
-            const file = event.target.files[0];
-            console.log(file);
-            setSelectedFile(file);
-      }
+      // const handleFileChange = (event) =>{
+      //       const file = event.target.files[0];
+      //       console.log(file);
+      //       setSelectedFile(file);
+      // }
     
       return (
         <div className="post-card">
@@ -48,8 +48,9 @@ const PostCard = ({setIsFullScreen}) => {
           </div>
 
           <div className='sec-3'>
-            <label htmlFor="fileInput"><PermMediaIcon /></label>
-            <input type="file" id="fileInput" style={{display: 'none'}} onChange={handleFileChange} />
+            <PermMediaIcon />
+            {/* <label htmlFor="fileInput"><PermMediaIcon /></label> */}
+            {/* <input type="file" id="fileInput" style={{display: 'none'}} onChange={handleFileChange} /> */}
             <EventAvailableIcon />
             <CelebrationIcon />
             <MoreHorizIcon />
